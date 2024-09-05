e62f05e847c83edae39f2c876cf4e77d86244bbe
0xf45uy4aa <154081073+fasuyaaaPNG@users.noreply.github.com>
2024-09-05 12:36:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -720,7 +720,7 @@ def plot_combined_percentage_chart2(selected_symbols, title):
 def main():
     # load_logs()
     st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", ["Compare 20 Coins", "Compare BTCUSDT and BCHUSDT", "log 5 menit", "log 15 menit"])
+    selection = st.sidebar.radio("Go to", ["Compare 20 Coins", "Compare BTCUSDT and BCHUSDT"])
 
     if 'selected_symbols' not in st.session_state:
         st.session_state.selected_symbols = []
